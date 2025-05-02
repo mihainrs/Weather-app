a1d64f619957c14d3d3a0928d1a20f27b216d190
Mihai Niculae <85626030+mihainrs@users.noreply.github.com>
2025-05-02 13:55:22 +0300

    Fixing stuff up and adding security measures

--- __main__.py
+++ __main__.py
@@ -1,19 +1,23 @@
 import requests
+from decouple import config
 
-api_key = '30d98e5f8b3acdf830227a452bddf428'
-#input for the target city
-city = input('City name: ')
-#url for the target city's weather prognosis
+api_key = config('api_key')
+
+#Input for the target city
+city = input('City name: ').strip() 
+
+#URL for the target city's weather prognosis
 url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}'
-#response for status codes
+
+#Response for status codes
 res = requests.get(url)
 
 if res.status_code == 200:
     data = res.json()
     temp = data['main']['temp'] - 273.15 
     desc = data['weather'][0]['description']
-    print('Temperature: {t} C'.format(t=temp))
-    print('Description: {d}'.format(d=desc))
+    print(f'Temperature: {temp:.1f} C')
+    print(f'Description: {desc}')
 elif res.status_code == 401:
     print('Invalid API key.')
 elif res.status_code == 404:
@@ -21,4 +25,4 @@ elif res.status_code == 404:
 elif res.status_code == 429:
     print('Rate limit exceeded.')
 else:
-    print('Error getting data: ', res.json())
+    print('Error getting data:', res.json())
